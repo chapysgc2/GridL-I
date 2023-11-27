6a547b73a66543027dd0cb3fed500046e4c74fd1
Hazel Alain <hazelo235@gmail.com>
2023-11-26 21:32:28 -0600

    Initial Commit

---
+++ ListasGrids/ListasGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListasGridsApp.swift
+//  ListasGrids
+//
+//  Created by Hazel Alain on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ListasGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
