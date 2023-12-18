27531fe0fcf4fc2297293af96ea7bad042d276e0
Hazel Alain <hazelo235@gmail.com>
2023-12-18 17:09:02 -0600

    user defaults

--- ListasGrids/GridList.swift
+++ ListasGrids/GridList.swift
@@ -6,25 +6,52 @@
 //
 
 import SwiftUI
-
+import Combine
 struct GridList: View {
     //let gritItem = [
       //  GridItem(.adaptive(minimum: 100 ))
     //]
     
-    let gridItem : [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+   // let gridItem : [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
+    @ObservedObject var grid = ModeloColumnas()
     var body: some View {
         
         NavigationView{
             ScrollView {
-                LazyVGrid(columns: gridItem, spacing: 30 ){
+                LazyVGrid(columns: grid.gridItem, spacing: 30 ){
                     ForEach(lista) { item in
                         Text(item.emoji)
                             .font(.system(size: 80))
                     }
                 }
-            }
-        }.navigationTitle("Grids")
+            }.navigationTitle("Grids")
+                .toolbar {
+                    ToolbarItem {
+                        Menu("opciones") {
+                            Section {
+                                Button("1 columna") {
+                                    grid.columnas(num : 1)
+                                }
+                                Button("2 columnas") {
+                                    grid.columnas(num : 2)
+                                }
+                                Button("3 columnas") {
+                                    grid.columnas(num : 3)
+                                }
+                                Button("4 columnas") {
+                                    grid.columnas(num : 4)
+                                }
+                                Button("destruir userdefaults") {
+                                    
+                                    UserDefaults.standard.removeObject(forKey: "numero")
+                                }
+                            }
+                        }
+                    }
+                }
+        }
+         
     }
 }
 
---
+++ ListasGrids/ModeloColumnas.swift
@@ -0,0 +1,33 @@
+//
+//  ModeloColumnas.swift
+//  ListasGrids
+//
+//  Created by Hazel Alain on 16/12/23.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ModeloColumnas : ObservableObject{
+    
+    @Published var gridItem = [GridItem()]
+    
+    
+    func columnas(num : Int){
+        
+        gridItem = Array(repeating: .init(.flxible(maximum:80)), count: num)
+        UserDefaults.standard.setValue(num, forKey: "numero")
+    }
+    
+    init(){
+        
+        if let num = UserDefaults.standard.object(forKey: "numero") as? Int {
+            gridItem = Array(repeating: .init(.flexible(maximum:80)), count: num)
+
+        }
+        else{
+            gridItem = Array(repeating: .init(.flexible(maximum:80)), count: 1)
+        }
+    }
+}
